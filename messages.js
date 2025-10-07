9ab00b1c7d9661759e493c42e332cf3e363ac1bd
Aratox <160970328+Aratox-Official@users.noreply.github.com>
2025-10-07 08:31:10 -0400

    Update messages.js

--- messages.js
+++ messages.js
@@ -1,8 +1,13 @@
 const terminalMessages = [
-  "C:\\Users\\Administrator> node index.html",
-  "Initializing system...",
-  "Loading Aratox profile...",
-  "Fetching projects...",
-  "All systems ready.",
-  "Loading cool stuff..."
+  "C:\\Users\\Administrator> npm install -g http-server",
+  "C:\\Users\\Administrator\\AppData\\Roaming\\npm\\node_modules\\http-server\\bin\\http-server",
+  "+ http-server@14.1.1",
+  "added 15 packages in 3.5s",
+  "\n",
+  "C:\\Users\\Administrator> cd Documents",
+  "C:\\Users\\Administrator\\Documents> http-server",
+  "Starting up http-server, serving ./",
+  "Available on:",
+  "  http://127.0.0.1:8080",
+  "  http://192.168.0.0:8080",
 ];
